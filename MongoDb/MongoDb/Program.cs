ed12d83d08f0c146319750b65dc33bed1a143c4f
ybouissa <youssef.bouissa@gmail.com>
2021-12-12 09:22:44 +0200

    Add files via upload

---
+++ MongoDb/MongoDb/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using MongoDB.Driver;
+
+
+namespace MongoDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MongoClient dbClient = new MongoClient("mongodb://localhost:27017");
+            var dblist = dbClient.ListDatabases().ToList();
+            dblist.ForEach(x=>Console.WriteLine(x));
+            var mongodb = dbClient.GetDatabase("dbtest");
+            var students = mongodb.GetCollection<Student>("Students");
+            var majors = mongodb.GetCollection < Major > ("Majors");
+            Major engineeringMajor = majors.Find(m => m.Description.Equals("Engineering")).FirstOrDefault();
+            Student bill = students.Find(x => x.firstname.Equals("Bill")).FirstOrDefault();
+            bill.lastname = "Clinton";
+            students.ReplaceOne(x => x.firstname.Equals("Bill"), bill);
+            majors.DeleteOne(d => d.Description.Equals("Business"));
+        }
+    }
+}
---
+++ MongoDb/MongoDb/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDb
+{
+    public class Student
+    {
+       [BsonId]
+       [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public Major major { get; set; }
+    }
+}
